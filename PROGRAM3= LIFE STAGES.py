694ec1dc16d87c62370f89a2457632d300f7fcc0
ARVELYN RAMOS <ramosarvelyn@gmail.com>
2021-11-17 23:17:11 +0800

    This repository shows the life stage of a person once they enter their age.

---
+++ PROGRAM3= LIFE STAGES.py
@@ -0,0 +1,9 @@
+age = float(input("AGE OF THE PERSON: "))
+if age >=19:
+    print("19 AND ABOVE: ADULT ")
+elif 18:
+    print("18: DEBUT ")      
+elif 13<=age<=17:
+    print("13 to 17: TEENS ")    
+elif 0<=age<13:
+    print("0 to 12: KIDS ")
