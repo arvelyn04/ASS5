d85d313ddbfd2b0aa5ac4864965a28e2311cdb5d
ARVELYN RAMOS <ramosarvelyn@gmail.com>
2021-11-17 22:57:05 +0800

    This repository shows PUP's GRADING SYSTEM.

---
+++ PROGRAM1= PUP GRADING SYSTEM.py
@@ -0,0 +1,31 @@
+percentageEquivalent = float(input("INPUT GRADE: "))
+if percentageEquivalent >=97:
+    print("GRADE/MARK: 1.0 ")
+    print("DESCRIPTION: EXCELLENT ")
+elif 94<=percentageEquivalent<96:
+    print("GRADE/MARK: 1.25 ")
+    print("DESCRIPTION: EXCELLENT ")
+elif 91<=percentageEquivalent<94:
+    print("GRADE/MARK: 1.5 ")
+    print("DESCRIPTION: VERY GOOD ")    
+elif 88<=percentageEquivalent<91:
+    print("GRADE/MARK: 1.75 ")
+    print("DESCRIPTION: VERY GOOD ")      
+elif 85<=percentageEquivalent<88:
+    print("GRADE/MARK: 2.0 ")
+    print("DESCRIPTION: GOOD ")   
+elif 82<=percentageEquivalent<85:
+    print("GRADE/MARK: 2.25 ")
+    print("DESCRIPTION: GOOD ")  
+elif 78<=percentageEquivalent<82:
+    print("GRADE/MARK: 2.5 ")
+    print("DESCRIPTION: SATISFACTORY ")     
+elif 76<=percentageEquivalent<78:
+    print("GRADE/MARK: 2.75 ")
+    print("DESCRIPTION: SATISFACTORY ")  
+elif 65<=percentageEquivalent<75:  
+    print("GRADE/MARK: 5.0")
+    print("DESCRIPTION: FAILURE ")
+elif 75:
+    print("GRADE/MARK: 3.0 ")
+    print("DESCRIPTION: PASSING ");
