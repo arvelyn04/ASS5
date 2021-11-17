d06135e56f831e95b1c861a7b13d776644a5d993
ARVELYN RAMOS <ramosarvelyn@gmail.com>
2021-11-17 23:05:59 +0800

    This repository shows how i get the lowest number using if-else statement.

---
+++ PROGRAM2=LOWEST NUMBER.py
@@ -0,0 +1,16 @@
+print("INPUT 3 NUMBERS ")
+askNumber1 = int(input("First Number:\t "))
+askNumber2 = int(input("Second Number:\t "))
+askNumber3 = int(input("Third Number:\t "))
+
+def lowest(askNumber1,  askNumber2, askNumber3):
+        return lowest 
+if (askNumber1 <=askNumber2 ) and (askNumber1 <= askNumber3):
+         lowest =askNumber1
+         print(f"LOWEST NUMBER IS: {lowest}\t ")
+elif (askNumber2 <= askNumber1) and (askNumber2 <= askNumber3):
+         lowest =askNumber2
+         print(f"LOWEST NUMBER IS: {lowest}\t ")
+elif (askNumber3 <= askNumber1) and (askNumber3 <= askNumber2):
+        lowest =askNumber3
+        print(f"LOWEST NUMBER IS: {lowest}\t")
